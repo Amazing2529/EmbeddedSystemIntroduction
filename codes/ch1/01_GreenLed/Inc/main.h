57a2bfb203cc8f91f9790a3cf753f629a2134890
syYang <yangsyun@gmail.com>
2020-09-07 15:12:53 +0800

    更新：ledRed 改为 ledBlue

--- codes/ch1/01_GreenLed/Inc/main.h
+++ codes/ch1/01_GreenLed/Inc/main.h
@@ -29,7 +29,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l4xx_hal.h"
-#define P_ledRed GPIO_PIN_13
+#define P_ledBlue GPIO_PIN_13
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
